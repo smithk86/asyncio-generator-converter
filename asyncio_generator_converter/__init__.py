f686e01ae08da00812e490d820e3349b0a2f8ea1
Kyle Smith <smithk86@gmail.com>
2022-03-02 12:09:49 -0500

    replace run_in_executor with asyncio.to_thread

--- asyncio_generator_converter/__init__.py
+++ asyncio_generator_converter/__init__.py
@@ -1,21 +1,37 @@
 from __future__ import annotations
 
+
 import asyncio
+import atexit
 import logging
 import functools
-from concurrent.futures import ThreadPoolExecutor
-from functools import partial
 from typing import TYPE_CHECKING
 
 import janus
 
 if TYPE_CHECKING:
-    from typing import AsyncGenerator, Callable, Generator
+    from typing import AsyncGenerator, Callable, Generator, List
 
 
-__version__ = "0.1.2"
+__version__ = "0.1.3-dev"
 __all__ = ["__version__", "asyncio_generator_converter"]
 logger = logging.getLogger(__name__)
+tasks: List[asyncio.Task] = list()
+
+
+def _cleanup_tasks() -> None:
+    for _task in tasks:
+        if _task.done():
+            tasks.remove(_task)
+
+
+def _atexit_hook() -> None:
+    _cleanup_tasks()
+    if len(tasks) > 0:
+        print(f"{__name__} has {len(tasks)} task(s) still running")
+
+
+atexit.register(_atexit_hook)
 
 
 def asyncio_generator_converter(func) -> Callable:
@@ -27,35 +43,30 @@ def asyncio_generator_converter(func) -> Callable:
         finally:
             logger.debug(f"consumer done for {generator}")
 
-    async def _run_consumer(executor: ThreadPoolExecutor, func: Callable):
-        _loop: asyncio.AbstractEventLoop = asyncio.get_running_loop()
-        await _loop.run_in_executor(executor, func)
-
     @functools.wraps(func)
     async def wrapper(*args, **kwargs) -> AsyncGenerator:
-        _executor: ThreadPoolExecutor = ThreadPoolExecutor(max_workers=1)
         _generator: Generator = func(*args, **kwargs)
         _queue: janus.Queue = janus.Queue(maxsize=1)
-        _task: asyncio.Task = asyncio.create_task(
-            _run_consumer(_executor, partial(_consumer, _generator, _queue))
+        _task = asyncio.create_task(
+            asyncio.to_thread(_consumer, _generator, _queue),
+            name=f"{__name__}:{_generator}",
         )
+        tasks.append(_task)
+
+        while not _task.done():
+            try:
+                _data = await asyncio.wait_for(_queue.async_q.get(), 1)
+                yield _data
+            except asyncio.TimeoutError:
+                pass
+
+        # ensure all results have been yielded
+        while _queue.async_q.qsize() > 0:
+            yield await _queue.async_q.get()
 
         try:
-            while not _task.done():
-                try:
-                    _data = await asyncio.wait_for(_queue.async_q.get(), 1)
-                    yield _data
-                except asyncio.TimeoutError:
-                    pass
-            while _queue.async_q.qsize() > 0:
-                yield await _queue.async_q.get()
+            await _task
         finally:
-            # shutdown the executor
-            _executor.shutdown()
-            # throw an exception if it occured
-            if _task.done():
-                _task.result()
-            else:
-                _task.cancel()
+            _cleanup_tasks()
 
     return wrapper
--- test/test_converter.py
+++ test/test_converter.py
@@ -1,31 +1,15 @@
+import asyncio
+import time
 from os import urandom
 
 import pytest  # type: ignore
-from asyncio_generator_converter import asyncio_generator_converter
-
-
-class random_data_generator_object:
-    def __init__(self):
-        self.running = True
-
-    @asyncio_generator_converter
-    def __aiter__(self):
-        for i in range(100):
-            if self.running:
-                yield urandom(16)
+from asyncio_generator_converter import asyncio_generator_converter, _atexit_hook
 
 
 @asyncio_generator_converter
 def random_data_generator():
     for i in range(100):
         yield urandom(16)
-
-
-@asyncio_generator_converter
-def throw_exception():
-    yield urandom(16)
-    yield urandom(16)
-    raise Exception("this is an exception")
 
 
 @pytest.mark.asyncio
@@ -39,8 +23,18 @@ async def test_converter():
 
 @pytest.mark.asyncio
 async def test_request_early_terminate():
+    class _random_data_generator_object:
+        def __init__(self):
+            self.running = True
+
+        @asyncio_generator_converter
+        def __aiter__(self):
+            for i in range(100):
+                if self.running:
+                    yield urandom(16)
+
     _results = list()
-    _agenerator = random_data_generator_object()
+    _agenerator = _random_data_generator_object()
     async for _data in _agenerator:
         if len(_results) > 50:
             _agenerator.running = False
@@ -50,8 +44,14 @@ async def test_request_early_terminate():
 
 @pytest.mark.asyncio
 async def test_exception():
+    @asyncio_generator_converter
+    def _throw_exception():
+        yield urandom(16)
+        yield urandom(16)
+        raise Exception("this is an exception")
+
     with pytest.raises(Exception, match="this is an exception"):
-        async for _data in throw_exception():
+        async for _data in _throw_exception():
             pass
 
 
@@ -60,3 +60,54 @@ async def test_without_async():
     with pytest.raises(TypeError, match="'async_generator' object is not iterable"):
         for _data in random_data_generator():
             pass
+
+
+@pytest.mark.asyncio
+async def test_slow_generator():
+    @asyncio_generator_converter
+    def _slow_generator():
+        for i in range(100):
+            if i > 3:
+                break
+            time.sleep(2)
+            yield urandom(16)
+
+    _results = list()
+    async for _data in _slow_generator():
+        _results.append(_data)
+    assert len(_results) == 4
+
+
+@pytest.mark.asyncio
+async def test_multiple_converter(capfd):
+    @asyncio_generator_converter
+    def _slow_generator():
+        for i in range(100):
+            if i > 3:
+                break
+            time.sleep(2)
+            yield urandom(16)
+
+    async def _consume_generator(async_gen):
+        async for _ in async_gen:
+            pass
+
+    _consumers = list()
+    gen1 = _slow_generator()
+    await gen1.__anext__()
+    _consumers.append(_consume_generator(gen1))
+    gen2 = _slow_generator()
+    await gen2.__anext__()
+    _consumers.append(_consume_generator(gen2))
+    gen3 = _slow_generator()
+    await gen3.__anext__()
+    _consumers.append(_consume_generator(gen3))
+
+    _atexit_hook()
+    _atexit_hook_stdout, _ = capfd.readouterr()
+    assert (
+        _atexit_hook_stdout.strip()
+        == "asyncio_generator_converter has 3 task(s) still running"
+    )
+
+    await asyncio.gather(*_consumers)
